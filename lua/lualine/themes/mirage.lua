2f8ba8192945c5a5c953d2c476bac27a4374eec6
Mohamed-elg <mohamed.elguermat9@gmail.com>
2024-12-01 13:11:23 +0100

    fix lualine

--- lua/lualine/themes/mirage.lua
+++ lua/lualine/themes/mirage.lua
@@ -3,42 +3,43 @@ local config = require("mirage.config")
 local theme = {}
 
 local bg = config.transparent and "NONE" or colors.yellowDarker
-local badge = colors.badge
+local badge = colors.fgAlt
+local fg = colors.white
 
 theme.normal = {
 	a = { fg = colors.blue, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.white },
-	c = { bg = bg, fg = colors.white },
+	b = { bg = bg, fg = fg },
+	c = { bg = bg, fg = fg },
 }
 
 theme.insert = {
 	a = { fg = colors.green, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	b = { bg = bg, fg = fg },
 }
 
 theme.terminal = {
 	a = { fg = colors.green, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	b = { bg = bg, fg = fg },
 }
 
 theme.command = {
 	a = { fg = colors.orange, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	b = { bg = bg, fg = fg },
 }
 
 theme.visual = {
 	a = { fg = colors.purple, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	b = { bg = bg, fg = fg },
 }
 
 theme.replace = {
 	a = { fg = colors.redDark, bg = badge, gui = "bold" },
-	b = { bg = bg, fg = colors.comment },
+	b = { bg = bg, fg = fg },
 }
 
 theme.inactive = {
 	a = { fg = colors.comment, bg = badge },
-	b = { bg = bg, fg = colors.yellow, gui = "bold" },
+	-- b = { bg = bg, fg = colors.yellow, gui = "bold" },
 	-- c = { bg = bg, fg = colors.yellow },
 }
 
